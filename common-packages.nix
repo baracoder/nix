044418bbaf545afe95263bbc9675b3cca427f380
System administrator <root@hex.fritz.box>
2016-04-26 23:31:14 +0200

    add hex config

--- common-packages.nix
+++ common-packages.nix
@@ -5,23 +5,30 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    avahi
     bcache-tools
     chromium
     curl
     dmenu
     docker
+    evince
+    firefox
+    gcc
     gitAndTools.gitFull
     gnumake
     i3
     i3lock
     i3status
     lightdm
+    lightdm_gtk_greeter
     mumble
     neovim
+    networkmanagerapplet
     numlockx
     pamixer
     pavucontrol
     pcmanfm
+    python
     roxterm
     slack
     spotify
---
+++ hex.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  boot.loader.gummiboot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = [
+    { name = "nixos-root"; device = "/dev/sda4";  allowDiscards = true; }
+    { name = "gentoo-root"; device = "/dev/sda2";  allowDiscards = true; }
+  ];
+  hardware.trackpoint.emulateWheel = true;
+  networking.hostName = "hex"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  environment.systemPackages = with pkgs; [
+    xorg.xbacklight
+    qtcreator
+  ];
+  virtualisation.docker.enable = true;
+  system.stateVersion = "16.03";
+}
