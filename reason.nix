169291188eb868da1c515214930c96e29969baa3
Herman Fries <hfries@wundercart.de>
2016-05-19 15:23:16 +0200

    reason: set docker to use overlayfs

--- reason.nix
+++ reason.nix
@@ -7,7 +7,10 @@
   networking.hostName = "reason";
   environment.systemPackages = with pkgs; [
   ];
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "overlay";
+  };
   services.xserver.videoDrivers = [ "nvidia" ];
   system.stateVersion = "16.03";
   powerManagement.powerUpCommands="/var/run/current-system/sw/sbin/hdparm -S 120 /dev/sd[a-z]";
