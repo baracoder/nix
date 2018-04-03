415db7a352d1c8924aebec3694ebb39c245fa3a2
Herman Fries <h.fries@nyris.io>
2018-04-03 16:22:52 +0200

    Remove samba server for a reason

--- reason.nix
+++ reason.nix
@@ -38,21 +38,4 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   system.stateVersion = "17.09";
   powerManagement.powerUpCommands="/var/run/current-system/sw/sbin/hdparm -S 120 /dev/sd[a-z]";
-
-
-
-  # samba for windows share
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    shares = {
-      homeBara = {
-        "read only" = false;
-        browseable = "yes";
-        comment = "home directory";
-        "guest ok" = "no";
-        path = "/home/bara";
-      };
-    };
-  };
 }
