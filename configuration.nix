b8da49e4f2dd86ee24dac1900bd854c1790d291e
Herman Fries <baracoder@googlemail.com>
2020-01-31 17:19:03 +0100

    Add vscode-with-extensions package

--- configuration.nix
+++ configuration.nix
@@ -182,6 +182,25 @@ in
     zsh
     #wineFull
     samba
+    (vscode-with-extensions.override {
+        # When the extension is already available in the default extensions set.
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.Nix
+          ms-kubernetes-tools.vscode-kubernetes-tools
+          redhat.vscode-yaml
+          vscodevim.vim
+
+        ]
+        # Concise version from the vscode market place when not available in the default set.
+        ++ vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "codestream";
+            publisher = "CodeStream";
+            version = "6.0.1";
+            sha256 = "0q6k3g2brmk5nf09z846d4whgv99rjmr928qzagqnv0fv1n9fyjf";
+          }
+        ];
+      })
 
     #springLobby
     #(buildFHSUserEnv {
