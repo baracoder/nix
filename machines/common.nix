4f5548357c8b90773b04b16be654c4de29c07f17
Herman Fries <baracoder@googlemail.com>
2021-04-27 18:00:53 +0200

    Remove brql driver

--- machines/common.nix
+++ machines/common.nix
@@ -83,7 +83,7 @@
     openssh.forwardX11 = true;
     udisks2.enable = true;
     printing.enable = true;
-    printing.drivers = with pkgs; [ gutenprint brlaser brgenml1lpr brgenml1cupswrapper (pkgs.callPackage ../pkgs/brql.nix {}) ];
+    printing.drivers = with pkgs; [ gutenprint brlaser brgenml1lpr brgenml1cupswrapper ];
     xserver =
     let xkbVariant = "altgr-intl"; # no dead keys
         xkbOptions = "eurosign:e,compose:menu,lv3:caps_switch";
