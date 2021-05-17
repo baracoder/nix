9a96969895cd2cca2bf2df6fa6b2d93ed820161f
Herman Fries <baracoder@googlemail.com>
2021-05-17 16:31:56 +0200

    Switch to work ovarlay

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.ny.url = "git+ssh://git@git.nyris.io:10022/nyris/ny?ref=main";
+  inputs.ny.url = "git+ssh://git@github.com/baracoder/nyris-nix-overlay?ref=main";
   inputs.nixos-hardware.url = github:NixOS/nixos-hardware/master;
 
   outputs = { self, nixpkgs, ny, nixos-hardware}: {
@@ -8,7 +8,7 @@
         hex = nixpkgs.lib.nixosSystem rec {
             system = "x86_64-linux";
             modules = [ 
-                ny.nixosModules.${system}.ny
+                ny.nixosModules.ny
                 ./machines/common.nix
                 ./machines/hex.nix
                 nixpkgs.nixosModules.notDetected
@@ -19,7 +19,7 @@
         hal = nixpkgs.lib.nixosSystem rec {
             system = "x86_64-linux";
             modules = [ 
-                ny.nixosModules.${system}.ny
+                ny.nixosModules.ny
                 ./machines/common.nix
                 ./machines/hal.nix
                 nixpkgs.nixosModules.notDetected
