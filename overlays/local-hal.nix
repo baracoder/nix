8b5e3b6ca3f0f37e123d0d14ad57ca82693ec726
Herman Fries <baracoder@googlemail.com>
2021-12-02 00:37:06 +0100

    hal: Remove nvidia patch from overlay

--- overlays/local-hal.nix
+++ overlays/local-hal.nix
@@ -1,16 +1,6 @@
 self: super:
 {
 
-    linuxPackagesFor = kernel:
-        (super.linuxPackagesFor kernel).extend (self': super': {
-        nvidiaPackages = super'.nvidiaPackages // { 
-            beta = super'.nvidiaPackages.beta.overrideAttrs (a': {
-                patches = [ ./nvidia-fix-linux-5.13.patch ];
-    
-            });
-        };
-    });
-
     prismatik = super.libsForQt5.callPackage ../pkgs/prismatik.nix {};
 
 }
