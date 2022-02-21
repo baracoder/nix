85b0ce47417f52ba3805533d2730f043389a3626
Herman Fries <baracoder@googlemail.com>
2022-02-21 10:49:33 +0100

    hal: Move to new disk and btrfs

--- machines/hal.nix
+++ machines/hal.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 let linuxPackages = pkgs.linuxPackages_zen;
-    nvidiaPackage = linuxPackages.nvidiaPackages.beta;
+    nvidiaPackage = linuxPackages.nvidiaPackages.stable;
 in
 {
   boot.loader.systemd-boot.enable = true;
@@ -9,28 +9,46 @@ in
   boot.kernelPackages = linuxPackages;
   boot.extraModulePackages = with linuxPackages; [ asus-wmi-sensors v4l2loopback ];
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod"  "nvme" "nvme_core" ];
-  boot.kernelModules = [ "kvm-intel" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" "nvidia" "asus-wmi-sensors" "v4l2loopback" ];
+  boot.kernelModules = [ "kvm-intel" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" "nvidia" "asus-wmi-sensors" "v4l2loopback" "btrfs" ];
 
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/65de6b98-348e-454f-a57a-d100cf19bd28";
+      fsType = "btrfs";
+      options = [ "discard=async" "relatime" "subvol=root" "compress=lzo" ];
+    };
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/65de6b98-348e-454f-a57a-d100cf19bd28";
+      fsType = "btrfs";
+      options = [ "discard=async" "relatime" "subvol=home" "compress=lzo" ];
+    };
 
-  hardware.cpu.amd.updateMicrocode = true;
-  hardware.bluetooth.enable = true;
-  hardware.steam-hardware.enable = true;
-
+  boot.initrd.luks.devices."crypt-ssd".device = "/dev/disk/by-uuid/c822c962-094c-45bc-bb24-ea57062f02a4";
   boot.initrd.luks.devices."crypt-ssd".allowDiscards = true;
 
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/4B1E-8899";
+      fsType = "vfat";
+      options = [ "relatime" ];
+    };
   swapDevices = [
     {
-      device = "/dev/disk/by-partlabel/cryptswap";
+      device = "/dev/disk/by-partlabel/crypt-swap";
       randomEncryption.enable = true;
     }
   ];
   systemd.units."dev-sdc2.swap".enable = false;
   systemd.generators.systemd-gpt-auto-generator = "/dev/null";
 
+
+  hardware.cpu.amd.updateMicrocode = true;
+  hardware.bluetooth.enable = true;
+  hardware.steam-hardware.enable = true;
+
   powerManagement.enable = true;
   powerManagement.cpuFreqGovernor = "ondemand";
 
-
+  virtualisation.waydroid.enable = true;
+  virtualisation.libvirtd.enable = true;
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
@@ -68,28 +86,9 @@ in
 
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.package = nvidiaPackage;
-  services.xserver.displayManager.gdm.nvidiaWayland = true;
   programs.xwayland.enable = true;
 
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/606839e6-36e4-41e4-8762-a8ccbe1704ce";
-      fsType = "ext4";
-      options = [ "discard" "noatime" ];
-    };
-
-  boot.initrd.luks.devices."crypt-ssd".device = "/dev/disk/by-uuid/4cee7fa9-8038-4da9-a176-624cdc89a515";
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/CDB5-AD45";
-      fsType = "vfat";
-      options = [ "noatime" ];
-    };
-
-
-
-  nix.maxJobs = lib.mkDefault 8;
-
+  nix.settings.max-jobs = lib.mkDefault 8;
 
   hardware.openrazer = {
     enable = true;
