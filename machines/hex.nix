1af756971dc4630366ec8412d206dd27884e8bcf
Herman Fries <baracoder@googlemail.com>
2022-05-11 13:14:03 +0200

    hex: Open RDP port

--- machines/hex.nix
+++ machines/hex.nix
@@ -19,6 +19,7 @@
 
   networking.hostName = "hex";
   networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [ 3389 ];
   environment.systemPackages = with pkgs; [
     xorg.xbacklight
   ];
