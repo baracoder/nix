fe1643e038284b295e9ce53ccf3d667c1d8afb2b
Herman Fries <baracoder@googlemail.com>
2022-11-05 12:51:51 +0100

    Enable experimental repl flake support

--- machines/common.nix
+++ machines/common.nix
@@ -7,7 +7,7 @@
   nix = {
     package = pkgs.nixUnstable;
     extraOptions = ''
-      experimental-features = nix-command flakes ca-derivations
+      experimental-features = nix-command flakes ca-derivations repl-flake
     '';
     settings = {
       trusted-users = [ "root" "@wheel" ];
