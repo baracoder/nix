1e175828e8a8b35162aad9abe6929cde5c57d00f
Herman Fries <baracoder@googlemail.com>
2022-12-28 17:54:00 +0100

    hex: Remove obsolete overrides

--- machines/hex.nix
+++ machines/hex.nix
@@ -10,9 +10,6 @@
     "nopti"
     "mem_sleep_default=s2idle"
     "nvme.noacpi=1"
-    # Workaround: https://gitlab.freedesktop.org/drm/intel/-/issues/6757#note_1602653
-    "i915.enable_psr=0"
-    "i915.enable_dc=0"
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -28,14 +25,6 @@
     jetbrains.datagrip
     jetbrains.rider
   ];
-  # power management
-  #services.thermald.enable = true;
-  #services.tlp.enable = false;
-  #powerManagement = {
-  #  enable = true;
-  #  cpuFreqGovernor = "powersave";
-  #};
-  powerManagement.powertop.enable = true;
 
   programs.nix-ld.enable = true;
 
@@ -78,38 +67,4 @@
   swapDevices = [ ];
 
   nix.settings.max-jobs = lib.mkDefault 8;
-
-
-  networking.extraHosts = ''
-# AD hosts Intercars
-#10.123.250.33 dcinter01.intercars.local dcinter01
-#10.123.250.35 dcinter02.intercars.local dcinter02
-#10.123.250.41 dcinter03.intercars.local dcinter03
-#10.123.250.40 dcinter04.intercars.local dcinter04
-#10.123.250.32 dcic03.ic dcic03
-#10.123.250.34 dcic04.ic dcic04
-#10.123.250.38 dcic05.ic dcic05
-#10.123.250.39 dcic06.ic dcic06
-##CDP Stage environment
-#10.125.3.4 cdp-stage-ldr-4.intercars.local cdp-stage-ldr-4
-#10.125.3.5 cdp-stage-ldr-5.intercars.local cdp-stage-ldr-5
-#10.125.3.6 cdp-stage-ldr-6.intercars.local cdp-stage-ldr-6
-## CDP prod environment
-#10.123.15.207 cdp-prod-ldr-1.intercars.local cdp-prod-ldr-1
-#10.123.15.208 cdp-prod-ldr-2.intercars.local cdp-prod-ldr-2
-#10.123.15.209 cdp-prod-ldr-3.intercars.local cdp-prod-ldr-3
-
-10.123.250.33   dcinter01.intercars.local       dcinter01
-10.123.250.35   dcinter02.intercars.local       dcinter02
-10.123.250.41   dcinter03.intercars.local       dcinter03
-10.123.250.40   dcinter04.intercars.local       dcinter04
-10.123.250.32   dcic03.ic       dcic03
-10.123.250.34   dcic04.ic       dcic04
-10.123.250.38   dcic05.ic       dcic05
-10.123.250.39   dcic06.ic       dcic06
-10.123.15.207   cdp-prod-ldr-1  cdp-prod-ldr-1.intercars.local
-10.123.15.208   cdp-prod-ldr-2  cdp-prod-ldr-2.intercars.local
-10.123.15.209   cdp-prod-ldr-3  cdp-prod-ldr-3.intercars.local
-  '';
-
 }
