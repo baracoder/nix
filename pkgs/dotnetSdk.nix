2f148a7fe5c7e1ed21d1b875d44820a2f33156b1
Herman Fries <baracoder@googlemail.com>
2023-02-01 13:55:47 +0100

    Remove dotnet sdk 5

--- pkgs/dotnetSdk.nix
+++ pkgs/dotnetSdk.nix
@@ -21,11 +21,9 @@ let myCombinePackages = packages:
 in myCombinePackages  (with dotnetCorePackages; [
     sdk_7_0
     sdk_6_0
-    sdk_5_0
     sdk_3_1
 
     aspnetcore_6_0
-    aspnetcore_5_0
     aspnetcore_3_1
 
 ])
