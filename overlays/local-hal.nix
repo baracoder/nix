aa6d8190f8297bec37384af1e6a98d97b487511c
Herman Fries <baracoder@googlemail.com>
2023-05-21 14:35:42 +0200

    hal: Remove comment

--- overlays/local-hal.nix
+++ overlays/local-hal.nix
@@ -1,7 +1,5 @@
 self: super:
 {
-
-    #prismatik = super.libsForQt5.callPackage ../pkgs/prismatik.nix {};
     psensor = super.psensor.overrideAttrs (a: {
         patches = [ ../pkgs/psensor.diff ];
     });
