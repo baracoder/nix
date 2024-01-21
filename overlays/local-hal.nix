0c4d854b6c7466ae269c14f565b11b0c99ed64c1
Herman Fries <baracoder@googlemail.com>
2024-01-21 01:04:33 +0100

    hal: Remove orca-slicer overlay

--- overlays/local-hal.nix
+++ overlays/local-hal.nix
@@ -3,15 +3,5 @@ self: super:
     psensor = super.psensor.overrideAttrs (a: {
         patches = [ ../pkgs/psensor.diff ];
     });
-    orca-slicer = super.bambu-studio.overrideDerivation (old: {
-        name = "orca-slicer-1.8.1";
-        src = super.fetchFromGitHub {
-        owner = "SoftFever";
-        repo = "OrcaSlicer";
-        rev = "v1.8.1";
-        hash = "sha256-3aIVi7Wsit4vpFrGdqe7DUEC6HieWAXCdAADVtB5HKc=";
-        };
-    });
-
 
 }
