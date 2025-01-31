f9a58eaba49a3ff5c0c74213b14dbaad2163aa29
Herman Fries <baracoder@googlemail.com>
2025-01-31 23:10:34 +0100

    hal: Add nebula

--- machines/hal/default.nix
+++ machines/hal/default.nix
@@ -147,6 +147,7 @@ in
     freecad
     offload-game
     egpu
+    nebula
     pywincontrols
     hifiscan
   ];
@@ -185,4 +186,29 @@ in
     };
   };
   services.switcherooControl.enable = true;
+
+  services.nebula.networks.mesh = {
+    enable = true;
+    cert = "/etc/nebula/hal.crt";
+    key = "/etc/nebula/hal.key";
+    ca = "/etc/nebula/ca.crt";
+    lighthouses = [ "192.168.98.3" "192.168.98.2" ];
+    staticHostMap = {
+      "192.168.98.3" = [ "me.notho.me:4242" ];
+      "192.168.98.2" = [ "zebar.de:4242" ];
+    };
+    firewall = {
+      outbound = [ { port = "any"; proto = "any"; host = "any"; } ];
+      inbound = [ { port = "any"; proto = "any"; host = "any"; } ]; 
+    };
+    settings = {
+      punchy = {
+        punch = true;
+        delay = "1s";
+        respond = true;
+        respond_delay = "5s";
+      };
+    };
+  };
+
 }
