88276185bfebd70abe658ddb94e5964b85bf8d97
Herman Fries <baracoder@googlemail.com>
2026-01-04 14:54:00 +0100

    hal: Add separtate steamlib subvolume

--- machines/hal/filesystems.nix
+++ machines/hal/filesystems.nix
@@ -25,6 +25,17 @@
       "compress=lzo"
     ];
   };
+  fileSystems."/home/bara/steamlibrary" = {
+    device = "/dev/disk/by-uuid/65de6b98-348e-454f-a57a-d100cf19bd28";
+    fsType = "btrfs";
+    options = [
+      "discard=async"
+      "relatime"
+      "nofail"
+      "subvol=steamlibrary"
+      "compress=lzo"
+    ];
+  };
 
   boot.initrd.luks.devices."crypt-ssd".device =
     "/dev/disk/by-uuid/c822c962-094c-45bc-bb24-ea57062f02a4";
