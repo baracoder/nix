63363d2dc5dcdf82cb7f5d8efa0450bd128026d7
Herman Fries <baracoder@googlemail.com>
2026-07-11 01:50:08 +0200

    hhd: Workaround python 3.14 incompatibility

--- overlays/local.nix
+++ overlays/local.nix
@@ -11,4 +11,13 @@ final: prev:
   vivaldi = prev.vivaldi.override {
     commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation";
   };
+  # Until next verion relesed
+  handheld-daemon = prev.handheld-daemon.overrideAttrs (oldAttrs: {
+    patches = oldAttrs.patches ++ [
+      (final.fetchurl {
+        url = "https://patch-diff.githubusercontent.com/raw/hhd-dev/hhd/pull/334.diff";
+        hash = "sha256-FCX6a6NT7UtIve/sRlKuic2jOKrnjP9sjAR6Lg6Ujcw=";
+      })
+    ];
+  });
 }
